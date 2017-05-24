b146449868703c366dba8843f922020ae287b80e
Ian Harrier <github@harrier.us>
2017-05-23 20:55:41 -0500

    Fix reverse proxy configuration

--- images/bitbucket/docker-entrypoint.sh
+++ images/bitbucket/docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 # This script modifies itself after successfully preforming tasks, preventing
 # the tasks from running every time the container is restarted.
 PERMISSIONS_COMPLETE=false
-PROXY_COMPLETE=false
 TIMEZONE_COMPLETE=false
 
 if [ "$PERMISSIONS_COMPLETE" = "false" ]; then
@@ -14,14 +13,6 @@ if [ "$PERMISSIONS_COMPLETE" = "false" ]; then
   sed -i 's/^PERMISSIONS_COMPLETE=.*/PERMISSIONS_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
 fi
 
-if [ "$PROXY_COMPLETE" = "false" ]; then
-  if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
-    echo "[I] Configuring Catalina to operate behind a reverse proxy."
-    sed -i "s/\(<Connector\)/\1 proxyName=\"$PROXY_HOSTNAME\" proxyPort=\"$PROXY_PORT\" scheme=\"$PROXY_SCHEME\"/" ${BITBUCKET_INSTALL}/conf/server.xml
-  fi
-  sed -i 's/^PROXY_COMPLETE=.*/PROXY_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
-fi
-
 if [ "$TIMEZONE_COMPLETE" = "false" ]; then
   if [ "$TIMEZONE" ]; then
     echo "[I] Setting the time zone."
@@ -29,6 +20,22 @@ if [ "$TIMEZONE_COMPLETE" = "false" ]; then
     echo "$TIMEZONE" > /etc/timezone
   fi
   sed -i 's/^TIMEZONE_COMPLETE=.*/TIMEZONE_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
+fi
+
+if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
+  echo "[I] Configuring Catalina to operate behind a reverse proxy."
+  : ${CATALINA_OPTS:=}
+  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyName=${PROXY_HOSTNAME}"
+  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyPort=${PROXY_PORT}"
+  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorScheme=${PROXY_SCHEME}"
+  if [ "$PROXY_SCHEME" = "http" ]; then
+    CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=false"
+  elif [ "$PROXY_SCHEME" = "https" ]; then
+    CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=true"
+  else
+    echo "[E] Invalid option for WEB_PROXY_SCHEME."
+    exit 1
+  fi
 fi
 
 echo "[I] Entrypoint tasks complete. Starting Bitbucket."
