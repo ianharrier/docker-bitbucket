39c8f8d6d17fdedc2cbfc02d0b6b39c0849e310c
Ian Harrier <github@harrier.us>
2017-05-24 21:53:02 -0500

    Fix reverse proxy configuration for Bitbucket 5.0+

--- images/bitbucket/docker-entrypoint.sh
+++ images/bitbucket/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # This script modifies itself after successfully preforming tasks, preventing
 # the tasks from running every time the container is restarted.
 PERMISSIONS_COMPLETE=false
+PROXY_COMPLETE=false
 TIMEZONE_COMPLETE=false
 
 if [ "$PERMISSIONS_COMPLETE" = "false" ]; then
@@ -13,6 +14,35 @@ if [ "$PERMISSIONS_COMPLETE" = "false" ]; then
   sed -i 's/^PERMISSIONS_COMPLETE=.*/PERMISSIONS_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
 fi
 
+if [ "$PROXY_COMPLETE" = "false" ]; then
+  PROPERTIES_FILE="${BITBUCKET_HOME}/shared/bitbucket.properties"
+  if [ ! -d "${BITBUCKET_HOME}/shared" ]; then
+    mkdir -p "${BITBUCKET_HOME}/shared"
+  fi
+  if [ ! -f $PROPERTIES_FILE ]; then
+    touch $PROPERTIES_FILE
+  fi
+  sed -i '/^server.proxy-name=.*/d' $PROPERTIES_FILE
+  sed -i '/^server.proxy-port=.*/d' $PROPERTIES_FILE
+  sed -i '/^server.scheme=.*/d' $PROPERTIES_FILE
+  sed -i '/^server.secure=.*/d' $PROPERTIES_FILE
+  if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
+    echo "[I] Configuring Bitbucket to operate behind a reverse proxy."
+    echo "server.proxy-name=$PROXY_HOSTNAME" >> $PROPERTIES_FILE
+    echo "server.proxy-port=$PROXY_PORT" >> $PROPERTIES_FILE
+    echo "server.scheme=$PROXY_SCHEME" >> $PROPERTIES_FILE
+    if [ "$PROXY_SCHEME" = "http" ]; then
+      echo "server.secure=false" >> $PROPERTIES_FILE
+    elif [ "$PROXY_SCHEME" = "https" ]; then
+      echo "server.secure=true" >> $PROPERTIES_FILE
+    else
+      echo "[E] Invalid option for WEB_PROXY_SCHEME."
+      exit 1
+    fi
+  fi
+  sed -i 's/^PROXY_COMPLETE=.*/PROXY_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
+fi
+
 if [ "$TIMEZONE_COMPLETE" = "false" ]; then
   if [ "$TIMEZONE" ]; then
     echo "[I] Setting the time zone."
@@ -20,22 +50,6 @@ if [ "$TIMEZONE_COMPLETE" = "false" ]; then
     echo "$TIMEZONE" > /etc/timezone
   fi
   sed -i 's/^TIMEZONE_COMPLETE=.*/TIMEZONE_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
-fi
-
-if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
-  echo "[I] Configuring Catalina to operate behind a reverse proxy."
-  : ${CATALINA_OPTS:=}
-  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyName=${PROXY_HOSTNAME}"
-  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyPort=${PROXY_PORT}"
-  CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorScheme=${PROXY_SCHEME}"
-  if [ "$PROXY_SCHEME" = "http" ]; then
-    CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=false"
-  elif [ "$PROXY_SCHEME" = "https" ]; then
-    CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=true"
-  else
-    echo "[E] Invalid option for WEB_PROXY_SCHEME."
-    exit 1
-  fi
 fi
 
 echo "[I] Entrypoint tasks complete. Starting Bitbucket."
